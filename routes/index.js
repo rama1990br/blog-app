5d547a2a5553f05f4fe63fe2678563a19e989e5f
rama <rama1990.br@gmail.com>
2018-11-17 14:59:59 -0800

    Initial commit towards UI components.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,35 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	var blog = fs.readFileSync('./blogs/blog.txt');
-  res.render('index', { blog: blog});
+	var products = [
+		{
+			"id": 1,
+			"src": "./public/images/carousel-1.jpg",
+			"alt": "product 1",
+		},
+
+		{
+			"id": 2,
+			"src": "./public/images/carousel-2.jpg",
+			"alt": "product 2",
+		},
+
+		{
+			"id": 3,
+			"src": "./public/images/carousel-3.jpg",
+			"alt": "product 3",
+		},
+
+		{
+			"id": 4,
+			"src": "./public/images/carousel-4.jpg",
+			"alt": "product 4",
+		},
+	];
+  res.render('index', { blog: blog, start: 1, count: 20, total: 2571, products: products});
+
+
 });
+
 
 module.exports = router;
